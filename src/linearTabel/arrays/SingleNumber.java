978eb9b848c4555f775c5356daea5bf61475ca5c
szfan <szfan1989@gmail.com>
2014-11-06 17:52:47 +0800

    add Candy and Single Number I & II

---
+++ src/linearTabel/arrays/SingleNumber.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2014 Alibaba.com All right reserved. This software is the
+ * confidential and proprietary information of Alibaba.com ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Alibaba.com.
+ */
+package linearTabel.arrays;
+
+/**
+ * 类SingleNumber.java的实现描述：TODO 类实现描述 
+ * @author zefan.szf 2014年11月6日 下午5:10:56
+ */
+public class SingleNumber {
+    public int singleNumber(int[] A) {
+        int result = 0;
+        for(int i = 0; i < A.length; ++i) {
+            result = result ^ A[i];
+        }
+        return result;
+    }
+    public static void main(String... args) {
+        int[] A = {1, 2, 2, 3, 4, 3, 1};
+        SingleNumber sn = new SingleNumber();
+        System.out.println(sn.singleNumber(A));
+    }
+}
---
+++ src/linearTabel/arrays/SingleNumberII.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2014 Alibaba.com All right reserved. This software is the
+ * confidential and proprietary information of Alibaba.com ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Alibaba.com.
+ */
+package linearTabel.arrays;
+
+/**
+ * 类SingleNumberOOII.java的实现描述：TODO 类实现描述 
+ * @author zefan.szf 2014年11月6日 下午5:43:24
+ */
+public class SingleNumberII {
+    public int singleNumber(int[] A) {
+        int one = 0, two = 0, three = 0;
+        for(int i = 0; i < A.length; ++i) {
+            two |= one & A[i];
+            one ^= A[i];
+            three = ~(one & two);
+            one &= three;
+            two &= three;
+        }
+        return one;
+    }
+    public static void main(String... args) {
+        
+    }
+}
